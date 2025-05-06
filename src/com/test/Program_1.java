698dfac7a7584d67b0758002c1c2e03032d881f7
Appu sindya B <151737486+Appushindhe@users.noreply.github.com>
2025-05-06 09:32:54 +0530

    Update Program_1.java

--- src/com/test/Program_1.java
+++ src/com/test/Program_1.java
@@ -4,6 +4,11 @@ public class Program_1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		Scanner s = new Scanner(System.in);
+		//Square of a number.
+		System.out.print("Enter a number:")
+			int n = s.nextInt();
+		System.out.print("Square of"+n+"+n*n);
 	}
 
 }
